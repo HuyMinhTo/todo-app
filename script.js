61cf56fc300783a6689190c3759809dd2bb2f5d8
hto2s <huy.to@smail.inf.h-brs.de>
2025-04-11 14:44:20 +0200

    feat: Todo-Task checkable/uncheckable

--- script.js
+++ script.js
@@ -5,23 +5,43 @@ const list = document.getElementById("todo-list");
 
 //Beim Hinzufügen eines neuen To-Do-Elements
 button.addEventListener("click", () => {
-   
     //Text aus dem Input-Feld trimmen und in Variable "taskText" speichern
     const taskText = input.value.trim();
 
     //Überprüfen, ob der Text nicht leer ist
     if(taskText !== ""){
-        
-        //Neues Li-Element erstellen und Text hinzufügen
+        //Neues Li-Element erstellen
         const newItem = document.createElement("div");
         newItem.classList.add("todo-item");
-        newItem.textContent = taskText;
 
-        //Li-Element zur Liste hinzufügen
+        //Neues Checkbox-Element erstellen
+        const checkbox = document.createElement("input");
+        checkbox.classList.add("todo-checkbox");
+        checkbox.type = "checkbox";
+
+        //Text-Element erstellen
+        const span = document.createElement("span");
+        span.textContent = taskText;
+        span.classList.add("todo-text");
+
+        //Checkbox Event Listener
+        checkbox.addEventListener("click", () => {
+            if(checkbox.checked){
+                span.style.textDecoration = "line-through";
+            }else{
+                span.style.textDecoration = "none";
+            }
+        });
+
+        //Elemente zusammenfügen
+        newItem.appendChild(span);
+        newItem.appendChild(checkbox);
         list.appendChild(newItem);
 
         //Input-Feld leeren
         input.value = "";
     }
-
 });
+
+
+
